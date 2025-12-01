fe0bc91111d97ee425261ba3959554f1f44ce46f
Gabinson200 <adamkocsis11@gmail.com>
2025-12-01 14:04:25 -0500

    day 1 riddle 1 fixed

--- day1.cpp
+++ day1.cpp
@@ -1,26 +1,35 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
 #include <iostream>
 #include <fstream> 
 using namespace std;
 
-// left and right combinations are in filename.txt
 
-int main(){
+int main()
+{
     string myText;
-    ifstream MyReadFile("filename.txt");
+    ifstream MyReadFile("test.txt");
     int pos = 50;
     int count = 0;
 
     while (getline (MyReadFile, myText)) {
+        std::cout << myText << " : ";
         if(myText.at(0) == 'L'){
             pos -= std::stoi(myText.substr(1,2));
-            if(pos < 0) pos = 99 - (pos % 99);
+            if(pos < 0) pos = 100 - abs(pos % 99);
         }else if(myText.at(0) == 'R'){
             pos += std::stoi(myText.substr(1,2));
-            if(pos > 99) pos = (pos % 99);
+            if(pos > 99) pos = (pos % 100);
         }
         
         if(pos == 0) count++;
-        //std::cout << pos << '\n';
+        std::cout << pos << '\n';
     }
 
     MyReadFile.close(); 
