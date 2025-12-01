145361dfdb1afd3d3bc3cfa51d7b410b101c9482
Gabinson200 <adamkocsis11@gmail.com>
2025-12-01 14:40:27 -0500

    day 1 riddle 1 done

--- day1.cpp
+++ day1.cpp
@@ -1,38 +1,34 @@
-/******************************************************************************
-
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
 #include <iostream>
 #include <fstream> 
-using namespace std;
+#include <string> // Include string for getline
+#include <cmath>  // Include cmath just in case
 
+using namespace std;
 
 int main()
 {
     string myText;
-    ifstream MyReadFile("test.txt");
+    ifstream MyReadFile("filename.txt"); 
+    
     int pos = 50;
     int count = 0;
 
-    while (getline (MyReadFile, myText)) {
-        std::cout << myText << " : ";
-        if(myText.at(0) == 'L'){
-            pos -= std::stoi(myText.substr(1,2));
-            if(pos < 0) pos = 100 - abs(pos % 99);
-        }else if(myText.at(0) == 'R'){
-            pos += std::stoi(myText.substr(1,2));
-            if(pos > 99) pos = (pos % 100);
+    while (getline(MyReadFile, myText)) {
+        // cout << "Read: " << myText << " : "; 
+        if (myText.at(0) == 'L') {
+            pos -= std::stoi(myText.substr(1));
+        } else if (myText.at(0) == 'R') {
+            pos += std::stoi(myText.substr(1));
         }
         
-        if(pos == 0) count++;
-        std::cout << pos << '\n';
+        pos = ((pos % 100) + 100) % 100;
+        if (pos == 0) count++;
+        // cout << pos << '\n';
     }
 
     MyReadFile.close(); 
 
-    return count;
+    cout << "Count: " << count << endl;
+
+    return 0;
 }
